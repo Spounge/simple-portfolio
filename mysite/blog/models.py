c38b43c8a3674c47d056f8cd0e3abcbfbe905f6d
Nelson ANGELIQUE <nelson.angelique@epitech.eu>
2019-07-23 18:40:52 +0200

    Added portfolio

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('publication date')
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
